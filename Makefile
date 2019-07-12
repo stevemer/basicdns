d4d7dfedc29da986208210d4ba60cc8cc89533a0
Steve Merritt <stevemerritt@stevemerritt-macbookpro2.roam.corp.google.com>
2019-07-12 00:53:40 -0500

    Initial impl of TCP client/server listening on correct DNS port.

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 server: server.cc
 	g++ server.cc -o server
+
+client: client.cc
+	g++ client.cc -o client
+
+clean:
+	rm server client
--- client.cc
+++ client.cc
@@ -1,64 +1,44 @@
-// Write CPP code here 
-#include <netdb.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
-  
-int main() 
-{ 
-    int sockfd, connfd; 
-    struct sockaddr_in servaddr, cli; 
-  
-    // socket create and varification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-  
-    // assign IP, PORT 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-    servaddr.sin_port = htons(PORT); 
-  
-    // connect the client socket to server socket 
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-        printf("connection with the server failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("connected to the server..\n"); 
-  
-    // function for chat 
-    func(sockfd); 
-  
-    // close the socket 
-    close(sockfd); 
-} 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PORT 53
+
+int main(int argc, char const * argv[]) {
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  const char * hello = "Hello from client";
+  char buffer[1024] = {0};
+
+  // Creating socket file descriptor.
+	// http://man7.org/linux/man-pages/man2/socket.2.html
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Socket creation error \n");
+    return -1;
+  }
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  // Cribbed shamelessly from the internet.
+  // Convert IPv4 and IPv6 addresses from text to binary form.
+  if (inet_pton(AF_INET, "127.0.0.1", & serv_addr.sin_addr) <= 0) {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
+
+	// Attempt to connect to the server.
+  if (connect(sock, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\nConnection Failed \n");
+    return -1;
+  }
+
+	// Send a simple message.
+	// TODO(stevemerritt): Whatever use cases we need beyond this.
+  send(sock, hello, strlen(hello), 0);
+  printf("Hello message sent\n");
+  valread = read(sock, buffer, 1024);
+  printf("%s\n", buffer);
+  return 0;
+}
